922f0d86947bdc995ce40c914daaea93bd5e2955
volter9 <volter925@gmail.com>
2015-10-20 11:22:02 -0700

    Initial commit: Project template

---
+++ Pomidorko/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Pomidorko
+//
+//  Created by Volter on 28.09.15.
+//  Copyright (c) 2015 volter9. All rights reserved.
+//
+// Red #ed4455
+// Blue #08b1cf
+// Settings #f7f7f7
+// About #fff
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pomidorko/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pomidorko
+//
+//  Created by Volter on 28.09.15.
+//  Copyright (c) 2015 volter9. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
